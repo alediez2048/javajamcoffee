eb7e3fbd47194517297adc8ddd6da3c7fb4815f3
Jorge Diez <alediez240a@Jorges-MacBook-Pro.local>
2017-10-23 15:41:25 -0500

    blah

---
+++ price.js
@@ -0,0 +1,12 @@
+var product1Price="30.00$"; 
+var product2Price="55.00$"; 
+var product3Price="105.00$"; 
+
+
+var simple=function(){
+   var textMultiple = {
+        text1:"text1",
+        text2:"text2"
+    };
+   return textMultiple;
+}
